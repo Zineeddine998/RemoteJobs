0c520d886cc92bb62d61e45acaf1e9c4e0c1bfc9
Zineeddine998 <55639522+Zineeddine998@users.noreply.github.com>
2020-07-15 01:52:44 +0100

    initial setup

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -5,7 +5,7 @@ import Zoom from "react-reveal/Zoom";
 const Listing = props => (
     <Zoom duration={700}>
         <div className={props.featured ? "listing listing--featured" : "listing"}>
-            <img className="listing__logo" src={'//logo.clearbit.com/' + props.company} alt="" style={{borderRadius: "50%",backgroundColor: "white"}}/>
+            { props.company ? <img className="listing__logo" src={'//logo.clearbit.com/' + props.company}style={{borderRadius: "50%",backgroundColor: "white"}}/> : <img className="listing__logo" src="https://www.ewebsolutionskech.com/web/nocache/data/pictures/fr/logo.png" style={{borderRadius: "50%",backgroundColor: "white"}}/>}
             <div className="listing__content">
                 <div className="listing__top-row">
                     <h2 className="listing__company">{props.company}</h2>
